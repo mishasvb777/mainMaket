e730a55d6eacf2f24a6007eab06e8a979b506970
mishasvb777 <121483075+mishasvb777@users.noreply.github.com>
2023-01-15 20:03:02 +0900

    Add files via upload

---
+++ scripts/mobile_menu.js
@@ -0,0 +1,13 @@
+let openMenu = document.querySelector('.header__menu');
+let closeMenu = document.querySelector('.header__back');
+let mobileMenu = document.querySelector('.body__mobileMenu');
+
+openMenu.addEventListener('click', function (){
+  mobileMenu.classList.add('body__mobileMenu-open');
+});
+
+closeMenu.addEventListener('click', function (){
+  mobileMenu.classList.remove('body__mobileMenu-open');
+});
+
+console.log(mobileMenu);
